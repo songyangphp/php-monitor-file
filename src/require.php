cb84e77c212eb056d94a0282b350ccb32acb12ac
songyang <475511896@qq.com>
2020-07-28 09:46:17 +0800

    songyang commit

---
+++ src/require.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__."/MonitorFile.php";
+require_once __DIR__."/MonitorTools.php";
+require_once __DIR__."/i/IMonitorFile.php";
+require_once __DIR__."/drive/GitMonitorFile.php";
+require_once __DIR__."/drive/Md5MonitorFile.php";
